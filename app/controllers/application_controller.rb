7cc5af557f18a6217bbf8b9122f19167101cd9a7
shoma <s.tone@genbasupport.com>
2025-10-14 14:15:35 +0900

    郵便番号を保存できるように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,6 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  before_action :configure_permitted_parameters, if: :devise_controller?
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:address, :self_introduction])
-  end
 end
