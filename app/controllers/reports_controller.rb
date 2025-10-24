232264e365d9c24918c1ca545cf1c970e1981a75
shoma <s.tone@genbasupport.com>
2025-10-24 17:20:14 +0900

    冗長な記述を修正

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -23,8 +23,7 @@ class ReportsController < ApplicationController
 
   # POST /reports or /reports.json
   def create
-    @report = Report.new(report_params)
-    @report.user = current_user
+    @report = current_user.report.build(report_params)
 
     respond_to do |format|
       if @report.save
