21951306a36fa24feb634937802ac8f48f7e7afb
shoma <s.tone@genbasupport.com>
2025-10-27 10:03:00 +0900

    N+1問題が発生していたので修正

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -10,6 +10,7 @@ class ReportsController < ApplicationController
   # GET /reports/1 or /reports/1.json
   def show
     @comment = Comment.new
+    @comments = @report.comments.includes(:user)
   end
 
   # GET /reports/new
