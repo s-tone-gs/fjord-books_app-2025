443dd1152ddd11f24afa87f745840862daf19925
shoma <s.tone@genbasupport.com>
2025-10-28 13:26:52 +0900

    不要な処理を削除

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,8 +12,6 @@ class CommentsController < ApplicationController
     respond_to do |format|
       if @comment.save
         format.html { redirect_to @commentable, notice: t('controllers.common.notice_create', name: Comment.model_name.human) }
-      else
-        format.html { refirect_to @commentable, status: :unprocessable_entity }
       end
     end
   end
