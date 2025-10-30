a3a47593e086980dea9f9884fc270ad257edc706
shoma <s.tone@genbasupport.com>
2025-10-30 15:26:29 +0900

    関連付けに必要な記述が抜けていたため修正

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,9 +4,9 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
   # 自分が言及元＝相手の言及先、なのでforeign_keyはmentioning_idになる
-  has_many :mentioned_relationship, class_name: 'Mention', foreign_key: 'mentioning_id', dependent: :destroy
+  has_many :mentioned_relationship, class_name: 'Mention', foreign_key: 'mentioning_id', dependent: :destroy, inverse_of: :mentioning
   has_many :mentioned_reports, through: :mentioned_relationship, source: :mentioned
-  has_many :mentioning_relationship, class_name: 'Mention', foreign_key: 'mentioned_id', dependent: :destroy
+  has_many :mentioning_relationship, class_name: 'Mention', foreign_key: 'mentioned_id', dependent: :destroy, inverse_of: :mentioned
   has_many :mentioning_reports, through: :mentioning_relationship, source: :mentioning
 
   validates :title, presence: true
