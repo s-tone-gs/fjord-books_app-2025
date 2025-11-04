cd6be85b15ea02967de89076c91c19f865ed482d
shoma <s.tone@genbasupport.com>
2025-11-04 15:48:07 +0900

    viewで行っていた処理をmodelに移譲

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,4 +3,8 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
+
+  def posted_by
+    user.name.empty? ? user.email : user.name
+  end
 end
