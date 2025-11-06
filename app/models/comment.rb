f82f0bc65ce5cae2d8e8a3218021436b2c36e91b
shoma <s.tone@genbasupport.com>
2025-11-06 17:39:14 +0900

    同じ処理を複数個所で定義していたのでまとめた

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,8 +5,4 @@ class Comment < ApplicationRecord
   belongs_to :user
 
   validates :body, presence: true
-
-  def posted_by
-    user.name.empty? ? user.email : user.name
-  end
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -3,8 +3,4 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
-
-  def posted_by
-    user.name.empty? ? user.email : user.name
-  end
 end
