3ce80e48941aa01560cf0b3232f97a2daa9aac2d
shoma <s.tone@genbasupport.com>
2025-11-14 10:41:17 +0900

    Userモデルのテストを作成

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,19 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'name_or_email return name when name is present' do
+    user = users(:just_registered_user)
+    # 念のためにテスト対象のデータのnameに値が存在するかをチェック
+    assert(user.name.present?, 'nameが存在しません。テストデータが間違っています')
+
+    assert_equal(user.name, user.name_or_email)
+  end
+
+  test 'name_or_email return email when name is not present' do
+    user = users(:just_registered_user_without_name)
+    # 念のためにテスト対象のデータのnameが空かをチェック
+    assert_not(user.name.present?, 'nameが空ではありません。テストデータが間違っています')
+
+    assert_equal(user.email, user.name_or_email)
+  end
 end
