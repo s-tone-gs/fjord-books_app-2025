36241d385ba1d89898f899a45ca5e155be1b3226
shoma <s.tone@genbasupport.com>
2025-11-18 16:48:06 +0900

    CRUD処理後のリソース表示に関するチェックが抜けていたため追加

--- test/system/reports_test.rb
+++ test/system/reports_test.rb
@@ -22,6 +22,10 @@ class ReportsTest < ApplicationSystemTestCase
     click_on I18n.t('helpers.submit.create', name: Report.model_name.human)
 
     assert_text I18n.t('controllers.common.notice_create', name: Report.model_name.human)
+    assert_text I18n.t('views.common.title_show', name: Report.model_name.human)
+    assert_text @report.title
+    assert_text @report.content
+    assert_text @report.user.name
     click_on I18n.t('views.common.back', name: Report.model_name.human)
   end
 
@@ -33,13 +37,19 @@ class ReportsTest < ApplicationSystemTestCase
     click_on I18n.t('helpers.submit.update', name: Report.model_name.human)
 
     assert_text I18n.t('controllers.common.notice_update', name: Report.model_name.human)
+    assert_text I18n.t('views.common.title_show', name: Report.model_name.human)
+    assert_text @report.title
+    assert_text @report.content
+    assert_text @report.user.name
     click_on I18n.t('views.common.back', name: Report.model_name.human)
   end
 
   test 'should destroy Report' do
     visit report_url(@report)
     click_on I18n.t('views.common.destroy', name: Report.model_name.human)
-
     assert_text I18n.t('controllers.common.notice_destroy', name: Report.model_name.human)
+
+    visit report_url(@report)
+    assert_text 'ActiveRecord::RecordNotFound'
   end
 end
